87f5950d88eb70671c69cf58f214b516402c3d3c
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-07-12 17:44:22 +0930

    Auth with email + password

---
+++ src/types/auth.types.ts
@@ -0,0 +1,4 @@
+export interface AuthEmailCredentialsType {
+    email: string,
+    password: string
+}
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,34 @@
 import { createClient } from '@supabase/supabase-js';
+import type { AuthEmailCredentialsType } from '../types/auth.types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// -- AUTH --
+export async function signup(user: AuthEmailCredentialsType) {
+  const { data, error } = await supabase.auth.signUp({
+    email: user.email,
+    password: user.password,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function login(user: AuthEmailCredentialsType) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: user.email,
+    password: user.password,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function logout() {
+    const {error} = await supabase.auth.signOut();
+    if (error) throw error;
+    return;
+}
