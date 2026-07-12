0d341bfdb71de81e0f74a0901d2ac4b75e0c350a
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-07-12 17:56:25 +0930

    Auth hooks

---
+++ src/hooks/auth.hooks.ts
@@ -0,0 +1,42 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { login, logout, signup } from "../utils/supabase";
+import toast from "react-hot-toast";
+
+export function useAuthSignup() {
+    return useMutation({
+        mutationFn: signup,
+        onSuccess: () => {
+            toast.success("Successfully signed up");
+        },
+        onError: (error:Error) => {
+            toast.error(error.message);
+        }
+    })
+}
+
+export function useAuthLogin() {
+    return useMutation({
+        mutationFn: login,
+        onSuccess: () => {
+            toast.success("Successfully logged in");
+        },
+        onError: (error:Error) => {
+            toast.error(error.message);
+        }
+    })
+}
+
+export function useAuthLogout() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: logout,
+        onSuccess: () => {
+            queryClient.clear();
+            toast.success("Successfully logged out");
+        },
+        onError: (error:Error) => {
+            toast.error(error.message);
+        }
+    })
+}
+
