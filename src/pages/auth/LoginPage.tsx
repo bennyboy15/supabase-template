749e5d0aaaa2b03064045befc182a988ec371363
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-07-12 19:47:26 +0930

    Login and Signup functionality

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,7 +1,23 @@
+import { useAuthLogin } from "../../hooks/auth.hooks"
+import type { AuthEmailCredentialsType } from "../../types/auth.types";
+import { useForm, type SubmitHandler } from "react-hook-form";
 
 function LoginPage() {
+    const { mutate: login } = useAuthLogin();
+    const { register, handleSubmit, } = useForm<AuthEmailCredentialsType>()
+    const onSubmit: SubmitHandler<AuthEmailCredentialsType> = (data) => {
+        login(data);
+    }
+
     return (
-        <div>LoginPage</div>
+        <div>
+            <div>LoginPage</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" {...register("email")} placeholder="example@email.com" />
+                <input type="password" {...register("password")} placeholder="password123" />
+                <button type="submit">LOGIN</button>
+            </form>
+        </div>
     )
 }
 
--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -1,7 +1,23 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { useAuthSignup } from "../../hooks/auth.hooks";
+import type { AuthEmailCredentialsType } from "../../types/auth.types";
 
 function SignupPage() {
+    const { mutate: signup } = useAuthSignup();
+    const { register, handleSubmit, } = useForm<AuthEmailCredentialsType>()
+    const onSubmit: SubmitHandler<AuthEmailCredentialsType> = (data) => {
+        signup(data);
+    }
+
     return (
-        <div>SignupPage</div>
+        <div>
+            <div>SignupPage</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" {...register("email")} placeholder="example@email.com" />
+                <input type="password" {...register("password")} placeholder="password123" />
+                <button type="submit">SIGN UP</button>
+            </form>
+        </div>
     )
 }
 
