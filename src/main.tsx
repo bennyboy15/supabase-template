725c9952b39bb6442c46df9b596518beaa6fe7cb
bennyboy15 <benjaminharvey2000@gmail.com>
2026-07-13 11:41:55 +0930

    React query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { Toaster } from "react-hot-toast"
-import { SessionProvider } from "./contexts/session.context.tsx"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "./contexts/session.context.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const client = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={client}>
-      <SessionProvider>
-        <App />
-        <Toaster />
-      </SessionProvider>
-    </QueryClientProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <QueryClientProvider client={client}>
+            <SessionProvider>
+                <App />
+                <Toaster />
+            </SessionProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    </StrictMode>,
+);
